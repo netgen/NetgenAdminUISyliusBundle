cea3530129b7484e3c298c67c8916f4838acb855
Edi Modrić <edi@netgen.hr>
2015-10-24 00:26:28 +0200

    Standard DI extension config

--- DependencyInjection/NetgenMoreAdminUISyliusExtension.php
+++ DependencyInjection/NetgenMoreAdminUISyliusExtension.php
@@ -1,31 +1,23 @@
 <?php
 
-/*
- * This file is part of the Sylius package.
- *
- * (c) Paweł Jędrzejewski
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 namespace Netgen\Bundle\MoreAdminUISyliusBundle\DependencyInjection;
 
-use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
-use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
-
+use Symfony\Component\DependencyInjection\Loader;
 
 class NetgenMoreAdminUISyliusExtension extends Extension
 {
     /**
-     * {@inheritdoc}
+     * {@inheritDoc}
      */
-    public function load(array $config, ContainerBuilder $container)
+    public function load( array $configs, ContainerBuilder $container )
     {
-        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('menu_plugins.yml');
+        $configuration = new Configuration();
+        $config = $this->processConfiguration( $configuration, $configs );
+
+        $loader = new Loader\YamlFileLoader( $container, new FileLocator( __DIR__ . '/../Resources/config' ) );
+        $loader->load( 'menu_plugins.yml' );
     }
 }
