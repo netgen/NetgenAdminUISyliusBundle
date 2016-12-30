10aba9f97c479437f976770b06e5d0386d721a47
Edi Modrić <edi@netgen.hr>
2016-12-30 18:26:24 +0100

    Update menu plugin to new admin UI and Sylius API

--- MenuPlugin/SyliusMenuPlugin.php
+++ MenuPlugin/SyliusMenuPlugin.php
@@ -8,7 +8,7 @@ use Symfony\Component\HttpFoundation\Request;
 class SyliusMenuPlugin implements MenuPluginInterface
 {
     /**
-     * Returns plugin identifier
+     * Returns plugin identifier.
      *
      * @return string
      */
@@ -18,7 +18,7 @@ class SyliusMenuPlugin implements MenuPluginInterface
     }
 
     /**
-     * Returns the list of templates this plugin supports
+     * Returns the list of templates this plugin supports.
      *
      * @return array
      */
@@ -26,21 +26,32 @@ class SyliusMenuPlugin implements MenuPluginInterface
     {
         return array(
             'aside' => 'NetgenAdminUISyliusBundle:menu/plugins/sylius:aside.html.twig',
-            'left' => 'NetgenAdminUISyliusBundle:menu/plugins/sylius:left.html.twig'
+            'left' => 'NetgenAdminUISyliusBundle:menu/plugins/sylius:left.html.twig',
         );
     }
 
     /**
-     * Returns if this plugin matches the current request
+     * Returns if the menu is active.
+     *
+     * @return bool
+     */
+    public function isActive()
+    {
+        return true;
+    }
+
+    /**
+     * Returns if this plugin matches the current request.
      *
      * @param \Symfony\Component\HttpFoundation\Request $request
      *
      * @return bool
      */
-    public function matches( Request $request )
+    public function matches(Request $request)
     {
-        // The sylius backend route names always start with "sylius_backend"
-        $currentRoute = $request->attributes->get( '_route' );
-        return ( stripos( $currentRoute, 'sylius_backend' ) !== false );
+        // The sylius admin route names always start with "sylius_admin"
+        $currentRoute = $request->attributes->get('_route');
+
+        return  stripos($currentRoute, 'sylius_admin') !== false;
     }
 }
