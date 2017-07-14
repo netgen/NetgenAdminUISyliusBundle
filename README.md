05e7378635e78630d672758d2d6d6b0f9aef74ac
Mario Blažek <blazek.mario@gmail.com>
2017-07-14 18:29:20 +0200

    Initial tests configuration

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 # Netgen Admin UI Sylius Bundle
 
+[![Build Status](https://img.shields.io/travis/netgen/NetgenAdminUISyliusBundle.svg?style=flat-square)](https://travis-ci.org/netgen/NetgenAdminUISyliusBundle)
+[![Code Coverage](https://img.shields.io/codecov/c/github/netgen/NetgenAdminUISyliusBundle.svg?style=flat-square)](https://codecov.io/gh/netgen/NetgenAdminUISyliusBundle)
+[![Quality Score](https://img.shields.io/scrutinizer/g/netgen/NetgenAdminUISyliusBundle.svg?style=flat-square)](https://scrutinizer-ci.com/g/netgen/NetgenAdminUISyliusBundle)
+[![Downloads](https://img.shields.io/packagist/dt/netgen/admin-ui-sylius-bundle.svg?style=flat-square)](https://packagist.org/packages/netgen/admin-ui-sylius-bundle/stats)
+[![Latest stable](https://img.shields.io/github/release/netgen/NetgenAdminUISyliusBundle.svg?style=flat-square)](https://github.com/netgen/NetgenAdminUISyliusBundle/releases)
+[![License](https://img.shields.io/packagist/l/netgen/admin-ui-sylius-bundle.svg?style=flat-square)](LICENSE)
+
 This bundle integrates Sylius into Netgen Admin UI by adding a menu plugin which loads the complete Sylius admin interface inside the Netgen Admin UI itself.
 
 ## Installation instructions
---
+++ Tests/MenuPlugin/SyliusMenuPluginTest.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Netgen\Bundle\AdminUISyliusBundle\Tests\MenuPlugin;
+
+use Netgen\Bundle\AdminUIBundle\MenuPlugin\MenuPluginInterface;
+use Netgen\Bundle\AdminUISyliusBundle\MenuPlugin\SyliusMenuPlugin;
+use PHPUnit\Framework\TestCase;
+use Symfony\Component\HttpFoundation\ParameterBag;
+use Symfony\Component\HttpFoundation\Request;
+
+class SyliusMenuPluginTest extends TestCase
+{
+    /**
+     * @var SyliusMenuPlugin
+     */
+    protected $plugin;
+
+    public function setUp()
+    {
+        $this->plugin = new SyliusMenuPlugin();
+    }
+
+    public function testInstanceOfMenuPlugin()
+    {
+        $this->assertInstanceOf(MenuPluginInterface::class, $this->plugin);
+    }
+
+    public function testGetIdentifier()
+    {
+        $this->assertEquals('sylius', $this->plugin->getIdentifier());
+    }
+
+    public function testIsActive()
+    {
+        $this->assertTrue($this->plugin->isActive());
+    }
+
+    public function testGetTemplates()
+    {
+        $expected = array(
+            'aside' => 'NetgenAdminUISyliusBundle:menu/plugins/sylius:aside.html.twig',
+            'left' => 'NetgenAdminUISyliusBundle:menu/plugins/sylius:left.html.twig',
+            'top' => 'NetgenAdminUISyliusBundle:menu/plugins/sylius:top.html.twig',
+        );
+
+        $actual = $this->plugin->getTemplates();
+
+        $this->assertTrue(is_array($actual));
+        $this->assertEquals($expected, $actual);
+    }
+
+    public function testMatches()
+    {
+        $request = new Request();
+        $request->attributes = new ParameterBag([
+            '_route' => 'sylius_admin',
+        ]);
+        $this->assertTrue($this->plugin->matches($request));
+
+        $request->attributes = new ParameterBag([
+            '_route' => 'test_sylius_admin_test',
+        ]);
+        $this->assertTrue($this->plugin->matches($request));
+
+        $request->attributes = new ParameterBag([
+            '_route' => 'sylius_admin_test',
+        ]);
+        $this->assertTrue($this->plugin->matches($request));
+    }
+
+    public function testDoesNotMatches()
+    {
+        $request = new Request();
+        $this->assertFalse($this->plugin->matches($request));
+
+        $request->attributes = new ParameterBag([
+            '_route' => 'test',
+        ]);
+        $this->assertFalse($this->plugin->matches($request));
+    }
+}
